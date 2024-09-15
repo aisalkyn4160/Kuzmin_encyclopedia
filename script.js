5a32988c0a73203ab2aed4b424d34149a5d102ff
aisalkyn4160 <aisalkyn.4160@gmail.com>
2024-09-15 15:06:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const header = document.querySelector('header');
+const catalogSection = document.querySelector('.section');
+let headerHeight = header.offsetHeight;
+catalogSection.style.marginTop = headerHeight + 'px'; 
